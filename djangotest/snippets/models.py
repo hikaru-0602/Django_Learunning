0330a6dc30ca75a40d84c8fa8944c01b0c949327
hikaru-0602 <beretta0602@gmail.com>
2024-08-12 00:38:57 +0900

    １章完了

--- djangotest/snippets/models.py
+++ djangotest/snippets/models.py
@@ -9,5 +9,30 @@ class Snippet(models.Model):
     created_at=models.DateTimeField('投稿日', auto_now_add=True)
     updated_at=models.DateTimeField('更新日', auto_now=True)
 
+    class Meta:
+        db_table='snippets'
+
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    text=models.TextField('本文', blank=False)
+    commented_at=models.DateTimeField('投稿日', auto_now_add=True)
+    commented_to=models.ForeignKey(Snippet, verbose_name='スニペット', on_delete=models.CASCADE)
+    commented_by=models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='投稿者', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table='comments'
+    
+    def __str__(self):
+        return f'{self.pk} {self.text}'
+    
+class Tag(models.Model):
+    name=models.CharField('タグ名', max_length=31)
+    snippets=models.ManyToManyField(Snippet, verbose_name='スニペット', related_name='tags')
+
+    class Meta:
+        db_table='tags'
+
+    def __str__(self):
+        return f'{self.pk} {self.name}'
