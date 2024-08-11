967e081cc9113634881ec013554c3b18476c0dde
hikaru-0602 <beretta0602@gmail.com>
2024-08-11 23:59:40 +0900

    ログイン・ログアウト・会員登録

--- djangotest/accounts/urls.py
+++ djangotest/accounts/urls.py
@@ -1,7 +1,14 @@
-from django.contrib.auth import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
 
 urlpatterns = [
+    path('signup/', CreateView.as_view(
+        template_name='accounts/signup.html',
+        form_class=UserCreationForm,
+        success_url='/'
+    ), name='signup'),
     path('login/', LoginView.as_view(
         redirect_authenticated_user=True,
         template_name='accounts/login.html'
--- djangotest/djangotest/settings.py
+++ djangotest/djangotest/settings.py
@@ -115,6 +115,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOGIN_URL = '/accounts/login'  #ログインページのURL
+LOGIN_REDIRECT_URL = '/'  #ログイン後のリダイレクト先のURL
+LOGOUT_REDIRECT_URL = '/'  #ログアウト後のリダイレクト先のURL
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
