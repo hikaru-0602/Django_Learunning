992d4e8141eac0e37b5f8eb84ce109db3cbe6ca0
hikaru-0602 <beretta0602@gmail.com>
2024-08-11 18:52:00 +0900

    登録・詳細・編集完成

--- djangotest/snippets/views.py
+++ djangotest/snippets/views.py
@@ -31,7 +31,7 @@ def snippet_new(request):
 @login_required
 def snippet_edit(request,snippet_id):
     snippet=get_object_or_404(Snippet, pk=snippet_id)
-    if snippet.created_by != request.user.id:
+    if snippet.created_by_id != request.user.id:
         return HttpResponse('このスニペットの編集は許可されていません')
     if request.method=='POST':
         form=SnippetForm(request.POST, instance=snippet)
